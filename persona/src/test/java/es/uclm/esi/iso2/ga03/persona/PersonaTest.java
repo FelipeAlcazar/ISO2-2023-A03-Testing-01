4011fd135f474f63fb34d18803aa0f769db97ac1
Manueehh <72602215+Manueehh@users.noreply.github.com>
2023-12-21 17:34:47 +0100

    Testing done

--- persona/src/test/java/es/uclm/esi/iso2/ga03/persona/PersonaTest.java
+++ persona/src/test/java/es/uclm/esi/iso2/ga03/persona/PersonaTest.java
@@ -3,8 +3,6 @@
  */
 package es.uclm.esi.iso2.ga03.persona;
 import static org.junit.Assert.*;
-
-import org.junit.Before;
 import org.junit.Test;
 
 import es.uclm.esi.iso2.ga03.persona.utility.Utility;
@@ -14,18 +12,26 @@ import java.util.Date;
 
 public class PersonaTest {
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
+    @Test
+    public void testIsLegalAge() throws ParseException {
+        Date dateOfBirthLegal = Utility.dateFormat.parse("01/01/2000");
+        Date dateOfBirthIllegal = Utility.dateFormat.parse("01/01/2010");
 
-	}
+        Persona legalPerson = new Persona("Legal", "Person", dateOfBirthLegal, "USA", "Education", "Cert", "123456789", "legal@example.com");
+        Persona illegalPerson = new Persona("Illegal", "Person", dateOfBirthIllegal, "USA", "Education", "Cert", "123456789", "illegal@example.com");
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+        assertTrue(legalPerson.isLegalAge());
+        assertFalse(illegalPerson.isLegalAge());
+    }
 
-	}
-    
+    @Test
+    public void testIsEuropean() throws ParseException {
+        Date dateOfBirth = Utility.dateFormat.parse("01/01/1990");
+
+        Persona europeanPerson = new Persona("European", "Person", dateOfBirth, "Spain", "Education", "Cert", "123456789", "european@example.com");
+        Persona nonEuropeanPerson = new Persona("Non-European", "Person", dateOfBirth, "Canada", "Education", "Cert", "123456789", "non.european@example.com");
+
+        assertTrue(europeanPerson.isEuropean());
+        assertFalse(nonEuropeanPerson.isEuropean());
+    }
 }
